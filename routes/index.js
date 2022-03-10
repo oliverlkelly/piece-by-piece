61c9c10746413303b504824ee9f6daeff78fcad4
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-10 21:55:29 +1100

    successfully testing page, need to do front end dev

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.render('index', {title: 'Sign Up Page Test'});
+    res.render('index', {title: 'Sign Up Page Test', title2: 'test2'});
 });
 
 module.exports = router;
