f71844303a160fc40035944e47be4111ce2f9bf0
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-10 21:09:58 +1100

    cleaning up modules and editing signup.js

--- signup.js
+++ signup.js
@@ -2,3 +2,9 @@ var express = require('express');
 var path = require('path');
 var handlebars = require('express-handlebars');
 
+var routes = require('./routes/index');
+
+var app = express();
+const hbs = handlebars.create()
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
