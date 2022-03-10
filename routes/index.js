fa62afb6ae5d1fe305a7381fd29a8a0cdefbaebc
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-10 21:15:09 +1100

    editing handlebarsfiles

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('./', function(req, res, next){
-    res.render('index', {title: 'Sign Up Page Test', condition: false, anyArray: [1,2,3]});
+router.get('/', function(req, res, next){
+    res.render('index', {title: 'Sign Up Page Test'});
 });
 
 module.exports = router;
--- signup.js
+++ signup.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var handlebars = require('express-handlebars');
 
-var routes = require('./routes/index');
+var routes = require('./routes');
 
 var app = express();
 const hbs = handlebars.create()
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(routes);
+app.listen(3001);
+
+
+//helloworld
