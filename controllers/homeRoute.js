50f7f3776f0f2af4ca9faa607499cde3b63a4646
Aiden Brennan <aiden.c.b@hotmail.com>
2022-03-20 22:53:16 +1100

    consolidated user scores (from per user per challenge to per user) when displayed in table

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -3,14 +3,24 @@ require('dotenv').config();
 
 const {  User, Challenge, Score } = require('../models');
 
+var mysql = require('mysql2')
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
 router.get('/', async function(req, res, next){
-    const challengeData = await Challenge.findAll();
-    const userchallenges = challengeData.map((chal) => chal.get({ plain: true }));
-    const user = {
-        firstname: "Oliver",
-        lastname: "Kelly"
-    }
-    res.render('homepage', {layout: 'main', title: 'Homepage', userchallenges, user, loggedIn: req.session.loggedIn});
+// const {  User, Challenge, Score} = require('../models');
+// // const withAuth = require('../utils/auth');
+
+// router.get('/', async function(req, res, next){
+//     const challengeData = await Challenge.findAll();
+//     const challenges = challengeData.map((chal) => chal.get({ plain: true }));
+//     res.render('homepage', {layout: 'main', title: 'Homepage', challenges, loggedIn: req.session.loggedIn});
+//     // res.status(200).json(challenges);
 });
 
 
@@ -26,14 +36,6 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/leaderboard', function(req, res, next){
-    var mysql = require('mysql2')
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
 
   con.connect(function(err) {
     if (err) throw err;
