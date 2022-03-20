bd5e342c1acf0d304210964a9e422f78c4557748
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-20 13:07:19 +1100

    finalising card structures and testing with dynamic data, need samer PR before full implimentation

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
-const {  User, Challenge } = require('../models');
+const {  User, Challenge, Score } = require('../models');
 
 router.get('/', async function(req, res, next){
     const challengeData = await Challenge.findAll();
-    const challenges = challengeData.map((chal) => chal.get({ plain: true }));
-    res.render('homepage', {layout: 'main', title: 'Homepage', challenges});
+    const userchallenges = challengeData.map((chal) => chal.get({ plain: true }));
+    const user = {
+        firstname: "Oliver",
+        lastname: "Kelly"
+    }
+    res.render('homepage', {layout: 'main', title: 'Homepage', userchallenges, user});
 });
 router.get('/signup', function(req, res, next){
     res.render('signup', {layout: 'main', title: 'Sign Up', isSignupPage: true});
